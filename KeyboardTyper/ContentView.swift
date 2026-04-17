bce80262b3806248bdf2c025bc2b7e4560073b00
ldy <26392985@qq.com>
2026-04-17 10:38:57 +0800

    Initial Commit

---
+++ KeyboardTyper/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KeyboardTyper
+//
+//  Created by LiYanDong on 2026-04-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KeyboardTyper/KeyboardTyperApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardTyperApp.swift
+//  KeyboardTyper
+//
+//  Created by LiYanDong on 2026-04-17.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardTyperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
